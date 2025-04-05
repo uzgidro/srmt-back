bf4163953bd3d2139c657925b42facb764b0d067
suxa56 <suxa5637@mail.ru>
2025-04-05 12:24:07 +0500

    cors

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: true,});
-  app.enableCors(
-    {
-      origin: 'https://srmt.speedwagon.uz',
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      preflightContinue: false,
-      optionsSuccessStatus: 204,
-    },)
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:4200', 'https://srmt.speedwagon.uz'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3100);
 }
 
